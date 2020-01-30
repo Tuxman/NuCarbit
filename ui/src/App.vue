32c1e84d15cd21a9bd2dd4871f4218cc77987ce7
Anthony Lusardi <anthony.lusardi@digitalasset.com>
2020-01-30 15:01:12 -0500

    space

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
+      <router-link to="/">Home</router-link> |
       <div v-if="party">
         Logged in as {{party}} |
 
