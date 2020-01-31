e128e4c2e4c038da3b89018d0103dc053e840a70
Anthony Lusardi <anthony.lusardi@digitalasset.com>
2020-01-31 14:59:52 -0500

    FIX: Webpack config

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -14,19 +14,6 @@ module.exports = {
 						loader: 'babel-loader',
 					},
 				],
-			},
-			transformAssetUrls: {
-				// video: ['src', 'poster'],
-				// source: 'src',
-				// img: 'src',
-				// image: 'xlink:href',
-				'b-img': 'src',
-				'b-img-lazy': ['src', 'blank-src'],
-				'b-card': 'img-src',
-				'b-card-img': 'src',
-				'b-card-img-lazy': ['src', 'blank-src'],
-				'b-carousel-slide': 'img-src',
-				'b-embed': 'src'
 			}
   }
 }
