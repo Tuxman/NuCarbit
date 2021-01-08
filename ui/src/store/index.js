c315fcd4e85ce74c832eac629dc7591eb821aa5a
Anthony Lusardi <anthony.lusardi@digitalasset.com>
2021-01-08 12:39:35 -0500

    Finish migration to @daml/ledger

--- ui/src/store/index.js
+++ ui/src/store/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import jwt from "jsonwebtoken";
-import axios from "axios";
 import createPersistedState from "vuex-persistedstate";
-import { ContractId,Party, Template } from '@daml/types';
+import { Choice, ContractId, Party, Template } from '@daml/types';
 import Ledger, { CreateEvent, Query, Stream, StreamCloseEvent, QueryResult } from '@daml/ledger';
+import * as obeer from '@daml.js/o_beer-1.0.0'
 
 Vue.use(Vuex);
 
@@ -17,18 +17,8 @@ export default new Vuex.Store({
     beersOwed: null,
     beerProposals: null,
     ledger: null,
-    ledgerUrl: process.env.VUE_APP_LEDGER_URL
-    // ledger: axios.create(
-    //   {
-    //       baseURL: process.env.NODE_ENV === 'production' ?
-    //         process.env.VUE_APP_LEDGER_URL
-    //         : window.location.origin,
-    //       timeout: 10000,
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       }
-    //     }
-    // )
+    ledgerUrl: process.env.VUE_APP_LEDGER_URL,
+    obeer: obeer
   },
   mutations: {
     loginParty(state, {party, token}){
@@ -55,24 +45,32 @@ export default new Vuex.Store({
       var token = null;
 
       if (typeof process.env.VUE_APP_TOKEN === 'undefined' || process.env.VUE_APP_TOKEN === null){
-        var payload = {"ledgerId": "o_beer", "applicationId": "HTTP-JSON-API-Gateway", "party": party};
+        // Local devmode
+        var payload = {
+          "https://daml.com/ledger-api": {
+            "ledgerId": "o_beer",
+            "applicationId": "HTTP-JSON-API-Gateway",
+            "actAs": [party]
+          }
+        };
         token = jwt.sign(payload, 'secret');
+      } else {
+        token = process.env.VUE_APP_TOKEN;
       }
 
       commit('loginParty', {party, token});
     },
     async getBeersOwed ({commit, state}) {
-      // var query = {
-      //   templateIds: ["Beer:Beer"],
-      //   query: {}
-      // };
+      var query = {
+        templateId: "Beer:Beer"
+      };
 
-      var beersOwed = await state.ledger.query(["Beer:Beer"], {});
-      commit('updateBeersOwed', beersOwed.contracts)
+      var beersOwed = await state.ledger.query(obeer.Beer.Beer);
+      commit('updateBeersOwed', beersOwed)
     },
     async getBeerProposals ({commit, state}) {
       var query = {
-        templateIds: ["Beer:BeerProposal"],
+        templateId: "Beer:BeerProposal",
         query: {
           beer: {
             recipient: this.party
@@ -80,33 +78,23 @@ export default new Vuex.Store({
         }
       };
 
-      var beerProposals = await state.ledger.post("/contracts/search", query)
-      commit('updateBeerProposals', beerProposals.data.result)
+      var beerProposals = await state.ledger.query(obeer.Beer.BeerProposal);
+      commit('updateBeerProposals', beerProposals)
     },
-    async exerciseChoice({ commit, state }, {templateId, contractId, choice, argument={}}) {
-      var query = {
-        templateId: templateId,
-        contractId: contractId,
-        choice: choice,
-        argument: argument
-      }
-
-      await state.ledger.post("/command/exercise", query)
+    async exerciseChoice({ commit, state }, {choice, contractId, argument={}}) {
+      await state.ledger.exercise(choice, contractId, argument);
     },
     async createBeerProposal({commit, state}, recipient) {
-      var query = {
-        templateId: "Beer:BeerProposal",
-        argument: {
+      var payload = {
           beer: {
-            templateId: "Beer:Beer",
+            templateId: obeer.Beer.Beer,
             giver: state.party,
             recipient: recipient
           }
-        }
-      };
+        };
 
       try {
-        await state.ledger.post("/command/create", query);
+        await state.ledger.create(obeer.Beer.BeerProposal, payload);
       }
       catch (err) {
         // TODO: Should throw an event and show a dialog box
