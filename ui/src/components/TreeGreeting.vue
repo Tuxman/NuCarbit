55ed411407b1040cd72dee545036c91b2a26beeb
jonbiveo <logantedmonds@gmail.com>
2022-02-18 14:12:05 -0700

    frontpage updated with cards

--- ui/src/components/TreeGreeting.vue
+++ ui/src/components/TreeGreeting.vue
@@ -2,14 +2,21 @@
   <v-card>
     <v-img 
     src="@/assets/tree-background.jpg"
-    class="white--text">
-      <v-card
-      max-width="200px"
-      
-      >
-        <v-card-text>Welcome to NuCarbit</v-card-text>
-
-      </v-card>
+    class="white--text d-flex">
+      <v-row>
+        <v-col cols="6">
+          <v-card
+          class="ma-6 d-inline-flex"
+          max-width="400px"
+          >
+            <v-card-text
+            class="d-flex justify-center"
+            >
+            Welcome to NuCarbit
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-img>
   </v-card>
 </template>
