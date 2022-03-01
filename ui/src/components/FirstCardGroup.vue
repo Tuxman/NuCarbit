490980f9954db7521c9ac1330a75cdddbdc2778c
tuxman <n.edmonds@topl.me>
2022-03-01 14:45:34 -0600

    Remove lorem ipsum from cards

--- ui/src/components/FirstCardGroup.vue
+++ ui/src/components/FirstCardGroup.vue
@@ -6,7 +6,6 @@
         <v-card class="rounded-xl mr-6" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>Privacy</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </v-card-text>
         </v-card>
       </v-col>
@@ -14,7 +13,6 @@
         <v-card class="rounded-xl mx-2" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>Security</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </v-card-text>
         </v-card>
       </v-col>
@@ -22,7 +20,6 @@
         <v-card class="rounded-xl ml-6" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>Correctness</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
           </v-card-text>
         </v-card>
       </v-col>
--- ui/src/components/SecondCardGroup.vue
+++ ui/src/components/SecondCardGroup.vue
@@ -6,7 +6,9 @@
         <v-card class="rounded-xl mr-6" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>Daml</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            	- Robust smart contract language and tooling for creating off-chain smart contracts
+              <br>
+              - can interface between multiple blockchains and databases
           </v-card-text>
         </v-card>
       </v-col>
@@ -14,7 +16,9 @@
         <v-card class="rounded-xl mx-2" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>Topl</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            	- Layer 1 blockchain focused on impact finance
+              <br>
+	            - smart assets allow full control of tokenized assets on-chain
           </v-card-text>
         </v-card>
       </v-col>
@@ -22,7 +26,7 @@
         <v-card class="rounded-xl ml-6" color="rgb(200, 200, 200, 0.95)">
           <v-card-title>ChainLink</v-card-title>
           <v-card-text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
+            - Read and write data from external sources
           </v-card-text>
         </v-card>
       </v-col>
