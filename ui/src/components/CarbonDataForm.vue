6026fc0765c3ac8a737b03120774a8e643cd0462
jonbiveo <logantedmonds@gmail.com>
2022-03-03 21:55:00 -0600

    updated minting page for on/off chain mints. Conditional rendering based on which button is pressed

--- ui/src/components/CarbonDataForm.vue
+++ ui/src/components/CarbonDataForm.vue
@@ -1,94 +1,85 @@
 <template>
-  <v-card>
-    <v-form>
-      <v-container>
-        <v-row class="d-flex justify-center">
-          <v-col cols="6">
-            <v-form ref="form">
-              <v-text-field label="Project ID">
-              </v-text-field>
-              <v-menu
-                v-model="carbonData.menu"
-                :close-on-content-click="false"
-                :nudge-right="40"
-                transition="scale-transition"
-                offset-y
-                min-width="auto"
-              >
-                <template v-slot:activator="{ on, attrs }">
-                  <v-text-field
+  <v-row justify="center">
+    <v-col cols="6">
+      <v-card>
+        <v-container>
+          <v-row justify="center">
+            <v-col cols="10">
+              <span>Where would you like to mint?</span>
+              <v-btn-toggle
+              v-model="toggleChain">
+                <v-btn value="on">On Chain</v-btn>
+                <v-btn value="off">Off Chain</v-btn>
+              </v-btn-toggle>
+              <v-form ref="form" v-show="toggleChain == 'on' || toggleChain == 'off'">
+                <v-text-field label="Project ID">
+                </v-text-field>
+                <v-menu
+                  v-model="carbonData.menu"
+                  :close-on-content-click="false"
+                  :nudge-right="40"
+                  transition="scale-transition"
+                  offset-y
+                  min-width="auto"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="carbonData.date"
+                      label="Issuance Date"
+                      prepend-icon="mdi-calendar"
+                      readonly
+                      v-bind="attrs"
+                      v-on="on"
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker
                     v-model="carbonData.date"
-                    label="Issuance Date"
-                    prepend-icon="mdi-calendar"
-                    readonly
-                    v-bind="attrs"
-                    v-on="on"
-                  ></v-text-field>
-                </template>
-                <v-date-picker
-                  v-model="carbonData.date"
-                  @input="carbonData.menu = false"
-                  scrollable
-                ></v-date-picker>
-              </v-menu>
-              <!-- <v-radio-group 
-              row
-              class="mt-n4 mb-n4"
-              v-model="radioDateGroup"
-              >
-                <v-radio
-                label="Year"
-                value="year"
-                ></v-radio>
-                <v-radio
-                label="Full Date"
-                value="fullDate"
-                ></v-radio>
-              </v-radio-group> -->
-              <v-text-field
-              label="Name of Project"
-              v-model="carbonData.projectName"
-              ></v-text-field>
-              <v-text-field 
-              label="Project Developer or Assembler"
-              v-model="carbonData.projectDeveloper"
-              ></v-text-field>
-              <v-text-field 
-              label="Project Type"
-              v-model="carbonData.projectType"
-              ></v-text-field>
-              <v-text-field 
-              label="Methodology"
-              v-model="carbonData.methodology"
-              ></v-text-field>
-              <v-text-field 
-              label="Total Carbon Credits Issued"
-              v-model="carbonData.creditsIssued"
-              ></v-text-field>
-              <v-text-field 
-              label="Location (Address, City/Town)"
-              v-model="carbonData.address"
-              ></v-text-field>
-              <v-text-field 
-              label="State/Province"
-              v-model="carbonData.state"
-              ></v-text-field>
-              <v-text-field 
-              label="Country"
-              v-model="carbonData.country"
-              ></v-text-field>
-              <v-btn @click="mockMint()">
-                mint
-              </v-btn>
-              <!-- <v-btn @click="mintData(carbonData.date,carbonData.projectName,carbonData.projectDeveloper,carbonData.projectType,carbonData.methodology,carbonData.creditsIssued,carbonData.address,carbonData.assetState,carbonData.country)">Mint</v-btn> --> 
-              <!-- {{ this.$store.state.carbonDataArr }}
-              {{ this.carbonData.projectName }} -->
-            </v-form>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-  </v-card>
+                    @input="carbonData.menu = false"
+                    scrollable
+                  ></v-date-picker>
+                </v-menu>
+                <v-text-field
+                label="Name of Project"
+                v-model="carbonData.projectName"
+                ></v-text-field>
+                <v-text-field 
+                label="Project Developer or Assembler"
+                v-model="carbonData.projectDeveloper"
+                ></v-text-field>
+                <v-text-field 
+                label="Project Type"
+                v-model="carbonData.projectType"
+                ></v-text-field>
+                <v-text-field 
+                label="Methodology"
+                v-model="carbonData.methodology"
+                ></v-text-field>
+                <v-text-field 
+                label="Total Carbon Credits Issued"
+                v-model="carbonData.creditsIssued"
+                ></v-text-field>
+                <v-text-field 
+                label="Location (Address, City/Town)"
+                v-model="carbonData.address"
+                ></v-text-field>
+                <v-text-field 
+                label="State/Province"
+                v-model="carbonData.state"
+                ></v-text-field>
+                <v-text-field 
+                label="Country"
+                v-model="carbonData.country"
+                ></v-text-field>
+                <v-btn @click="mockMint()">
+                  mint
+                </v-btn>
+              </v-form>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -100,7 +91,8 @@ export default {
     return {
       // radioDateGroup: 'year'
       carbonData: Object.assign(this.carbonDataObj),
-      transaction: ''
+      transaction: '',
+      toggleChain: ''
     }
   },
   methods: {
